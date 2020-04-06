31e2a3929baf04a8dc1f0758d136f4e7099b1a38
Robert Kulp <pedalinon@gmail.com>
2020-04-06 15:50:26 -0700

    build script success. Fixing refresh by replacing BrowserRouter with HashRouter

---
+++ portfolio/build/precache-manifest.16e33f33342670401190a25f2cbdba1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adf6129795b7e6836152ca290a0914e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19c0e36c8b22a57eb7a5",
+    "url": "/static/css/main.29b917fd.chunk.css"
+  },
+  {
+    "revision": "0c149593ca6d776a2671",
+    "url": "/static/js/2.8ec5f55c.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.8ec5f55c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c0e36c8b22a57eb7a5",
+    "url": "/static/js/main.b1d56b17.chunk.js"
+  },
+  {
+    "revision": "0b04740ca17586159bf4",
+    "url": "/static/js/runtime-main.cc57c104.js"
+  },
+  {
+    "revision": "8bf3b6df4283177c0adf93620d73984c",
+    "url": "/static/media/wheet_field.8bf3b6df.jpg"
+  }
+]);
