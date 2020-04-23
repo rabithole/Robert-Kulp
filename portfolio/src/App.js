e6a14b9d46ec47e7f7151637b9925c0284a9e6c8
Robert Kulp <pedalinon@gmail.com>
2020-04-22 21:26:15 -0700

    still working on loeader

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -22,26 +22,32 @@ import UpdateTrip from './components/UpdateTrip';
 import SingleTrip from './components/SingleTrip';
 import Spinner from './components/Spinner.js';
 
-let Loader = require('react-loader');
-// import CreateProfile from './components/CreateProfile';
+const Loader = require('react-loader');
 
 const jwtDecode = require('jwt-decode');
 
+let loaded = true;
+
 function App(props) {
   const [trips, setTrips] = useState([]);
-  const [loaded, setLoaded] = useState(true);
-  // console.log(trips);
+  // const [loaded, setLoaded] = useState(true);
   const signedIn = getToken();
 
   function isSignedIn() {
      const decoded = jwtDecode(localStorage.getItem('token'));
-      // console.log(decoded);
       trips.user_id = decoded.userid;
+  }
+
+  function startLoading() {
+      return loaded = false;
+    }
+
+  function stopLoading() {
+    return loaded = true;
   }
 
   function updateTrip() {
     axiosWithAuth()
-    
     .get('trips')
     .then(response => {
       setTrips(response.data)
@@ -57,130 +63,71 @@ function App(props) {
 
   useEffect(() => {
       !signedIn ? console.log('Signed In') : isSignedIn()
-      // setLoaded(false);
-      
-setTimeout(function(){
+      // startLoading();
+// setTimeout(function() {
       axiosWithAuth()
       .get('trips')
       .then(response => {
-        // setLoaded(true);        
+        console.log('then')
         setTrips(response.data)
+        
+        console.log(loaded)
       })
-
       .catch(error => {
         localStorage.removeItem('token');
         props.history.push('/Login');
         console.log(error)
       })
-}, 2000);
+// }, 4000)
     }, dependArray); 
-
-
+  
   return (
     <div className='App'>
+      <Loader loaded={loaded} scale={15.00} color='black'>
+          <Spinner />
+      </Loader>
 
      	<header>
       		<h1>A WORK IN PROGRESS</h1>
       		<h1>SOMETHING MORE IS COMING SOON!</h1>
      	</header>
 
-    <div id='guidr'>
-  <section>
-	    <h1>Guided Trips</h1>
-	    <h5>A Lambda School Project</h5>
-	</section>
+      <div id='guidr'>
+        <section>
+      	    <h1>Guided Trips</h1>
+      	    <h5>A Lambda School Project</h5>
+      	</section>
 
-      <nav>
-        {!signedIn && 
-          <Link 
-            to='/SignUp'>
-              Sign Up
-          </Link>}
+        <nav>
+          {!signedIn && <Link to='/SignUp'>Sign Up</Link>}
 
-        {!signedIn &&
-          <Link 
-            to='/Login'>
-              Login
-          </Link>}
+          {!signedIn &&<Link to='/Login'>Login</Link>}
 
-        {signedIn && 
-          <Link 
-            to='/CreateTrip'>
-              Add Trip
-          </Link>}
+          {signedIn && <Link to='/CreateTrip'>Add Trip</Link>}
 
-        {signedIn && 
-          <Link
-            to='/Trips'>
-              Trip List
-          </Link>
-        }
+          {signedIn && <Link to='/Trips'>Trip List</Link>}
 
-        {signedIn && 
-          <Link
-            to='/Logout'>
-              Logout
-          </Link>
-        }
-        {/*signedIn && 
-          <Link 
-            to='/CreateProfile'>
-              Create Profile
-          </Link>
-        */}
-      </nav>
+          {signedIn && <Link to='/Logout'>Logout</Link>}
+        </nav>
 
-{/*//////////////////////////////////////////////////*/}
-      <Loader loaded={loaded} scale={15.00} color='black'>
-        <Spinner />
-      </Loader>
+        <Route exact path='/login' component={Login}></Route>
+        
+        <Route exact path='/SignUp' component={SignUp}></Route>
+        
+        <ProtectedRoute exact path='/Logout' component={Logout}/>
 
-      <Route 
-        exact path='/login'
-        component={Login}
-      >
-      </Route>
-      
-      <Route 
-        exact path='/SignUp'
-        component={SignUp}
-      >
-      </Route>
-      
-      <TripsContext.Provider value={{ trips, updateTrip }}>
-        <ProtectedRoute
-          exact path='/Trips'
-          component={Trips}
-        />
+        <TripsContext.Provider value={{ trips, updateTrip }}>
+          <ProtectedRoute exact path='/Trips' component={Trips}/>
 
-        <ProtectedRoute
-          exact path='/Trip/:id'
-          component={Trip}
-        />
+          <ProtectedRoute exact path='/Trip/:id' component={Trip}/>
 
-        <ProtectedRoute
-          exact path='/SingleTrip/:id'
-          component={SingleTrip}
-        />
+          <ProtectedRoute exact path='/SingleTrip/:id' component={SingleTrip}/>
 
-        <ProtectedRoute
-          exact path='/UpdateTrip/:id'
-          component={UpdateTrip}
-        />
+          <ProtectedRoute exact path='/UpdateTrip/:id' component={UpdateTrip}/>
 
-        <ProtectedRoute
-        	exact path='/CreateTrip'
-        	component={CreateTrip}
-      	/>
-      </TripsContext.Provider>
-
-      <ProtectedRoute
-        exact path='/Logout'
-        component={Logout}
-      />
-
-    </div>
-
+          <ProtectedRoute exact path='/CreateTrip' component={CreateTrip}/>
+        </TripsContext.Provider>
+      </div>
     </div>
   );
 }
--- portfolio/src/utils/axiosWithAuth.js
+++ portfolio/src/utils/axiosWithAuth.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-export function getToken() {
-	// console.log(localStorage.getItem('token'));
-	
+export function getToken() {	
 	return localStorage.getItem('token')
 }
 
