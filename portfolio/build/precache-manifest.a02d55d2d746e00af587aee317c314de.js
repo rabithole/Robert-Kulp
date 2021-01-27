93afcca336cbf4998b89eb68f6e142cf39cbad72
Robert Kulp <pedalinon@gmail.com>
2021-01-27 14:25:54 -0800

    latest build

---
+++ portfolio/build/precache-manifest.a02d55d2d746e00af587aee317c314de.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bba32d3b98d2b22e57207db4233d2b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c19fe30c383a15b2029",
+    "url": "/static/css/main.45403244.chunk.css"
+  },
+  {
+    "revision": "edc2153a5f91ecd8703b",
+    "url": "/static/js/2.7a5cce3d.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.7a5cce3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c19fe30c383a15b2029",
+    "url": "/static/js/main.22469708.chunk.js"
+  },
+  {
+    "revision": "0b04740ca17586159bf4",
+    "url": "/static/js/runtime-main.cc57c104.js"
+  },
+  {
+    "revision": "25eb7b26d9835684f034d564b49a7573",
+    "url": "/static/media/New_Oowee_Banner.25eb7b26.png"
+  },
+  {
+    "revision": "02d54ced14ecea6cbbb63676092a0b99",
+    "url": "/static/media/alexbrush-regular-webfont.02d54ced.ttf"
+  },
+  {
+    "revision": "08308b574e30d3b99bdcc1e8d0ea5654",
+    "url": "/static/media/alexbrush-regular-webfont.08308b57.woff"
+  },
+  {
+    "revision": "6d5cf78360c2b7dbddaf0b15a0f78309",
+    "url": "/static/media/alexbrush-regular-webfont.6d5cf783.eot"
+  },
+  {
+    "revision": "ae632a5b52fa92062380dce42d1b5734",
+    "url": "/static/media/alexbrush-regular-webfont.ae632a5b.svg"
+  },
+  {
+    "revision": "82e33a1d41a8fc2a7470e704f28f8aef",
+    "url": "/static/media/backgroundcard.82e33a1d.jpg"
+  },
+  {
+    "revision": "08b1e5c20c146d03fe4a06b903a66142",
+    "url": "/static/media/bites_2.08b1e5c2.png"
+  },
+  {
+    "revision": "ba04eb024b2322103f333eb94c1bbebc",
+    "url": "/static/media/choc_with_shadow.ba04eb02.png"
+  },
+  {
+    "revision": "cfc0b5ec23a78013c76f7851d9c9e706",
+    "url": "/static/media/facebook_256_transparency.cfc0b5ec.png"
+  },
+  {
+    "revision": "491cd80dc3c31b91797475be96fe770b",
+    "url": "/static/media/logo1.491cd80d.png"
+  },
+  {
+    "revision": "d5cfd3034827675fa3f1ce9fced11f31",
+    "url": "/static/media/mt_st_helens.d5cfd303.jpg"
+  },
+  {
+    "revision": "fa6551cd203fd215f91310d0e9a19501",
+    "url": "/static/media/pinterest-icon.fa6551cd.png"
+  }
+]);
